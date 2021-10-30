b290e8e8e1b7b7c8bb8a60fa227fee25733c8107
Anna <acdodd17@gmail.com>
2021-10-30 10:57:36 -0400

    generate password length

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,26 @@
 // Assignment code here
+var generatePassword = function () {
+  console.log("New password being created!");
+  var newPassword = " ";
 
+  // prompt passowrd length
+  var getPassLength = function () {
+    var passLength = window.prompt(
+      "How long would you like your password to be? Please choose a length between 8 and 128"
+    );
+    passLength = parseInt(passLength);
+    if (passLength >= 8 && passLength <= 128) {
+      console.log(passLength);
+      return passLength;
+    } else passLength < 8 || passLength > 128 || undefined;
+    {
+      window.prompt("Enter a number of at least 8 and no more than 128!");
+    }
+  };
+
+
+  return newPassword;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
